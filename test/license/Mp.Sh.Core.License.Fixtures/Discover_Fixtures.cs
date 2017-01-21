6d089346569c758ccec4371e462c71c08efd6181
raffaeu@gmail.com <raffaeu@gmail.com>
2017-01-21 12:39:05 +0100

    First tests with xunit and Net Core

---
+++ test/license/Mp.Sh.Core.License.Fixtures/Discover_Fixtures.cs
@@ -0,0 +1,33 @@
+﻿/****************************** Module Header ******************************\
+Module Name:  <File Name>
+Project:      <Sample Name>
+Copyright (c) Mproof B.V.
+
+Last Edit: Raffaele Garofalo
+\***************************************************************************/
+
+using Xunit;
+
+namespace Mp.Sh.Core.License.Fixtures
+{
+    public class Discover_Fixtures
+    {
+        #region Public Constructors
+
+        public Discover_Fixtures()
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        [Fact]
+        public void Discover_Returns_GrantTypes()
+        {
+            Assert.True(true);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ test/license/Mp.Sh.Core.License.Fixtures/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mp.Sh.Core.License.Fixtures")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d40c5660-be2e-4971-aaef-768e26d8be90")]
